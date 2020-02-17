81c7de035d2a2e68f3336060121fa5f16be5e897
Devwrat <devwratjoshi1234@gmail.com>
2020-02-17 12:37:58 +0900

    Change directory structure

--- horizontal_bed_sims/plot_basic_shaking_initial_final.py
+++ horizontal_bed_sims/plot_basic_shaking_initial_final.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data_files = 5 #The number of data files
-folder = "/home/dev/sketchbookProcessing/Horizontal_Bed_Simulations/Basic_Shaking_Init_Final_Posns/data/"
+folder = "/home/dev/sketchbookProcessing/Horizontal_Bed_Simulations/Basic_Shaking_Init_Final_Posns/data/Yes_Friction_Yes_Size/Big_Low_Friction/"
 ext = ".txt" #Data file extention
 initial_points_low_friction = []
 initial_points_high_friction = []
@@ -14,7 +14,6 @@ fig,(ax_initial, ax_final) = plt.subplots(1,2, figsize=(15,10))
 
 for i in range(1, 5+1):
     data_initial = pd.read_csv(folder+"Initial/" + str(i) + ext, header=None, index_col=False)
-    print(folder + "Final/" + str(i) + ext)
     data_final = pd.read_csv(folder+"Final/" + str(i) + ext, header=None, index_col=False)
 
 
@@ -25,12 +24,17 @@ for i in range(1, 5+1):
     
 
 
-    ax_initial.scatter(initial_points_low_friction.values[:,0], initial_points_low_friction.values[:,1], color='green', marker='o')
-    ax_initial.scatter(initial_points_high_friction.values[:,0], initial_points_high_friction.values[:,1], color='red', marker='o')
-    ax_final.scatter(final_points_low_friction.values[:,0], final_points_low_friction.values[:,1], color='green', marker='o')
-    ax_final.scatter(final_points_high_friction.values[:,0], final_points_high_friction.values[:,1], color='red', marker='o')
+    ax_initial.scatter(initial_points_high_friction.values[:,0], initial_points_high_friction.values[:,1], color='red', marker='o', s=112.5)
+    ax_initial.scatter(initial_points_low_friction.values[:,0], initial_points_low_friction.values[:,1], color='green', marker='o', s=50)
+    ax_final.scatter(final_points_high_friction.values[:,0], final_points_high_friction.values[:,1], color='red', marker='o', s=112.5)
+    ax_final.scatter(final_points_low_friction.values[:,0], final_points_low_friction.values[:,1], color='green', marker='o', s=50)
 #plt.plot(initial_points_low_friction[:,0], initial_points_low_friction[:,1])
 #plt.plot(final_points_low_friction)
-
+ax_initial.set_title("Initial positions")
+ax_final.set_title("Final positions")
+ax_initial.set_xlabel("X position", fontsize='xx-large')
+ax_initial.set_ylabel("Y position", fontsize='xx-large')
+ax_final.set_xlabel("X position", fontsize='xx-large')
+ax_final.set_ylabel("Y position", fontsize='xx-large')
 plt.show()
 
